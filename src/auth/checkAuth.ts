b4eae1197e203f5d58de3afcd8c3930f467ca7ee
Tanesan <keito.12.10@icloud.com>
2022-09-15 12:25:26 +0900

    Pretterの修正

--- src/auth/checkAuth.ts
+++ src/auth/checkAuth.ts
@@ -1,7 +1,7 @@
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import '../firebase/init'
 
-const auth = getAuth();
-onAuthStateChanged(auth, user => {
-    return user;
-});
+const auth = getAuth()
+onAuthStateChanged(auth, (user) => {
+  return user
+})
--- src/auth/googleLogin.ts
+++ src/auth/googleLogin.ts
@@ -1,15 +1,16 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 import '../firebase/init'
 
-const auth = getAuth();
-const provider = new GoogleAuthProvider();
+const auth = getAuth()
+const provider = new GoogleAuthProvider()
 
 export const GoogleLogin = () => {
-signInWithPopup(auth, provider)
-  .then((result) => {
-    console.log("Login completed", result);
-    return result;
-  }).catch((error) => {
-    console.log(error, error.code, error.message);
-  })
-};
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log('Login completed', result)
+      return result
+    })
+    .catch((error) => {
+      console.log(error, error.code, error.message)
+    })
+}
