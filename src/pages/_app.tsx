f29d3938968368c53d5c305c4451aebd10fa7953
Tanesan <keito.12.10@icloud.com>
2022-09-15 13:33:41 +0900

    add / トップページ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,15 @@ import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
 import React from 'react'
 import '../firebase/init'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from '../mui/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,52 @@ import type { NextPage } from 'next'
 import React from 'react'
 // import Head from 'next/head'
 // import Image from 'next/image'
-import styles from '../../styles/Home.module.css'
 import '../firebase/init'
 import { GoogleLogin } from '../auth/googleLogin'
+import Box from '@mui/material/Box'
+import Fab from '@mui/material/Fab'
+import Container from '@mui/material/Container'
+import GoogleIcon from '@mui/icons-material/Google'
+import Typography from '@mui/material/Typography'
+import { Grid } from '@mui/material'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <button onClick={() => GoogleLogin()}>ログイン</button>
-    </div>
+    <Container
+      sx={{ bgcolor: 'background.default', width: '100%', height: '100vh' }}
+    >
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+      >
+        <Grid item xs={12}>
+          <Box sx={{ mt: 20 }}>
+            <Typography
+              variant="h1"
+              component="h1"
+              sx={{ color: 'text.primary' }}
+            >
+              MIMAMORI
+            </Typography>
+          </Box>
+          <Box sx={{ mt: 15 }}>
+            <Fab
+              onClick={() => GoogleLogin()}
+              variant="extended"
+              sx={{ bgcolor: 'white', textTransform: 'none' }}
+            >
+              <GoogleIcon sx={{ mr: 1 }} />
+              <Box sx={{ px: 1 }}>
+                <Typography variant="subtitle1">Googleでログイン</Typography>
+              </Box>
+            </Fab>
+          </Box>
+        </Grid>
+      </Grid>
+      <div className="tritangle"></div>
+    </Container>
   )
 }
 
