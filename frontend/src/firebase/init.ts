3a46c6f558eb60b48ec7d043b0e545cad726d7b3
Tanesan <keito.12.10@icloud.com>
2022-09-17 17:09:23 +0900

    firebase init のapi更新

--- frontend/src/firebase/init.ts
+++ frontend/src/firebase/init.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 
 // 実務ではこんなことすると怒られますがハッカソンなので許容すべき
 const firebaseConfig = {
-  apiKey: 'AIzaSyDvufiQB3YBxzxCFigXUCOmIvqgsKMPV5Y',
-  authDomain: 'hackday-2022-126c3.firebaseapp.com',
-  projectId: 'hackday-2022-126c3',
-  storageBucket: 'hackday-2022-126c3.appspot.com',
-  messagingSenderId: '992478333427',
-  appId: '1:992478333427:web:241ac68993a941718e6a86',
+  apiKey: "AIzaSyDnWbUlIl7hNhxByqCykUzj4i08ADEKjLU",
+  authDomain: "hackday-2022-126c3.firebaseapp.com",
+  projectId: "hackday-2022-126c3",
+  storageBucket: "hackday-2022-126c3.appspot.com",
+  messagingSenderId: "992478333427",
+  appId: "1:992478333427:web:241ac68993a941718e6a86"
 }
 
 export const app = initializeApp(firebaseConfig)
