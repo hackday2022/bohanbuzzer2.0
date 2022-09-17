f6f954d1a7139f623e15841dedf87e03282d798e
HosokawaR <thin.river.jp@gmail.com>
2022-09-17 19:56:09 +0900

    feat: add init db

--- frontend/src/firebase/init.ts
+++ frontend/src/firebase/init.ts
@@ -1,13 +1,17 @@
 import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
 // 実務ではこんなことすると怒られますがハッカソンなので許容すべき
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDnWbUlIl7hNhxByqCykUzj4i08ADEKjLU",
-  authDomain: "hackday-2022-126c3.firebaseapp.com",
-  projectId: "hackday-2022-126c3",
-  storageBucket: "hackday-2022-126c3.appspot.com",
-  messagingSenderId: "992478333427",
-  appId: "1:992478333427:web:241ac68993a941718e6a86"
+  apiKey: 'AIzaSyDnWbUlIl7hNhxByqCykUzj4i08ADEKjLU',
+  authDomain: 'hackday-2022-126c3.firebaseapp.com',
+  projectId: 'hackday-2022-126c3',
+  storageBucket: 'hackday-2022-126c3.appspot.com',
+  messagingSenderId: '992478333427',
+  appId: '1:992478333427:web:241ac68993a941718e6a86',
 }
 
 export const app = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
