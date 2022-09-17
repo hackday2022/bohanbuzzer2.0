6e80659ab3d7799121df41db758d0b30aba78cfa
HosokawaR <thin.river.jp@gmail.com>
2022-09-17 19:55:47 +0900

    feat: add types

---
+++ frontend/src/types.ts
@@ -0,0 +1,59 @@
+import { CommonFirestore } from '@common'
+import {
+  DocumentData,
+  DocumentReference,
+  SnapshotOptions,
+  Timestamp,
+  QueryDocumentSnapshot,
+} from 'firebase/firestore'
+
+import * as t from 'io-ts'
+
+const TimestampIots = new t.Type<Timestamp, Timestamp, unknown>(
+  'Timestamp',
+  (u): u is Timestamp => u instanceof Timestamp,
+  (u, c) => (u instanceof Timestamp ? t.success(u) : t.failure(u, c)),
+  (a) => a
+)
+
+const DocumentReferenceIoto = new t.Type<
+  DocumentReference,
+  DocumentReference,
+  unknown
+>(
+  'DocumentReference',
+  (u): u is DocumentReference => u instanceof DocumentReference,
+  (u, c) => (u instanceof DocumentReference ? t.success(u) : t.failure(u, c)),
+  (a) => a
+)
+
+export namespace Firestore {
+  export const converter = <A extends DocumentData, O, I>(
+    type: t.Type<A, O, I>
+  ) => ({
+    toFirestore: (data: any): DocumentData => {
+      if (!type.is(data)) {
+        console.error(data)
+        throw new Error('Invalid data type.')
+      }
+      return data
+    },
+    fromFirestore: (
+      snapshot: QueryDocumentSnapshot,
+      options?: SnapshotOptions
+    ): A => {
+      const data = snapshot.data(options)
+      if (!type.is(data)) {
+        console.error(data)
+        throw new Error('Invalid data type.')
+      }
+      return data
+    },
+  })
+
+  export const Child = CommonFirestore.getChild(DocumentReferenceIoto)
+
+  export const Device = CommonFirestore.getDevice(TimestampIots)
+
+  export const Warning = CommonFirestore.getWarning(TimestampIots)
+}
